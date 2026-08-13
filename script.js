f3f9e5f3c16577cc490406267007803cb3cda671
Akash2-2kumar <106278593+Akash2-2kumar@users.noreply.github.com>
2026-08-13 11:18:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('', (input) => {
+    const num = Number(input);
+
+    if(num%400==0){
+		console.log(366);
+	}else if(num%100==0){
+		console.log(365);
+	}else if(num%4==0){
+		console.log(366);
+	}else{
+		console.log(365);
+	}
+
+    rl.close();
+});
